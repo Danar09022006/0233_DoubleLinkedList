af758c0a15f531ff818390570b58ea645d323b20
Danar09022006 <rohmanrozaqi8@gmail.com>
2025-05-13 12:14:46 +0700

    node step 1

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -19,18 +19,17 @@ Node *START = NULL;
 // deklarasi prosedur addNode
 void addNode()
 {
-
+    // membuat node dan pemberian value untuk data noMhs dan name
+    Node *newNode = new Node; // step 1 buat Node baru
+    cout << "\nEnter the roll number of the student: ";
+    cin >> newNode->noMhs; // Assign value to the data field of the new node
+    cout << "\nEnter the name of the student: ";
+    cin >> newNode->name; // Assign value to the data fied of th
     
     
     
     
     
-    
-
-    // insert the new node in the list
-    // kondisi jika star == null atau noMhs node baru <= noMhs start
-    if (START == NULL || newNode->noMhs <= START->noMhs)
-    {
         // step 2: insert the new node at the beginning
         // kondisi jika start tidak kosong dan noMhs node baru sama dengan noMhs
         if (START != NULL && newNode->noMhs == START->noMhs)
