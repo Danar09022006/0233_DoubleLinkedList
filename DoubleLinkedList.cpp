9a16981ceb5be1133a171668c0b0476cfcc236df
Danar09022006 <rohmanrozaqi8@gmail.com>
2025-05-13 11:50:52 +0700

    pembuatan fanction

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -46,6 +46,182 @@ void addNode()
         if (START != NULL)
         {
             START->prev = newNode; // step 4: make the  frist node point to the new node
+        
+        }
+        // membuat nilai pref = null dan start = node baru
+        newNode->prev = NULL; // step 5: make the new node point to null
+        START = newNode; // step 6: make the new node the frist node
+    }
+    // kondisi jika semua kondisi if tidak terpenuhi
+    else
+    {
+
+        // insert the new node in the middle or at the end
+        // set nilai current = start dan nilai previous = null
+        Node *current = START; // step 1.a: start from the frist node
+        Node *previous = NULL; // step 1.b: previous node is null initially
+
+        // looping selama current != null dan noMhs dari current lebih kecil dan tidak sama dengan noMhs node baru
+        while (current != NULL && current->noMhs < newNode->noMhs)
+        {                               // step 1.c: traverse the list
+            previous = current;         // step 1.d: move the previous to the current
+            current = current->next;    // step 1.e: move to the next node
+        }
+
+        // set nilai next node baru = current dan prev node baru = previous
+        newNode->next = current; // step 4: make the next field of the new node
+        newNode->prev = previous; // step 5: make the previous field of the new node
+
+        // kondisi jika current tidak sama dengan null
+        if (current != NULL)
+        {
+            current->prev = newNode; //step 6: make the previous fieid of the current
+        }
+        
+        // kondisi jika previous tidak sama dengan null
+        if (previous != NULL)
+        {
+            previous->next = newNode; // step 7: make the new field of the previous node
+
+        }
+        // kondisi jika previous  sama dengan null
+        else
+        {
+            // if previous is still null, if means newNode is now the first node
+            START = newNode; 
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+// pembuatan prosedur delete untuk menghapus data
+void *previous, *current;
+int rollNo;
+
+cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+cin >> rollNo; // step 3: get the roll number to be deleted
+
+if (START == NULL)
+{
+    cout << "List is empty" << endl;
+    return;
+} 
+
+current = START; // step 1: start from the first node
+previous = NULL;
+
+// locate the node to be deleted
+while (current != NULL && current->noMhs != rollNo)
+{
+    previous = current;
+    current = current->next;
+}
+
+if (current == NULL)
+{
+    cout << "\033[31mThe record whith roll number " << rollNo << " not found\033[0m" << endl;
+    return;
+}
+
+// node to be deleted is the first node
+if (current = START)
+{
+    START = START->next; // step 2: update the START pointer
+    if (START != NULL)
+    {
+        START->prev = NULL;
+    }
+}
+else
+{ // node to be deleted is not the first node
+    previous->next = current->next;
+    if (current->next != NULL)
+    {  // if there's a successor, update its previous pointer
+        current->next->prev = previous;
+    }
+
+    // relase the memory of the node marked as current
+    delete current;
+    cout << "\x1b[32mRecord with roll number " << rollNo << " deleted\x1b[0m" << endl;
+ 
+}
+    // method untuk mengecek apakah list kosong
+    bool kistEmpty()
+    {
+        return (START == NULL);
+
+    }
+    // prosedur traverse untuk menampilkan data
+    void traverse()
+    {
+        if (listEmpty())
+           cout << "\nList is empty" << endl;
+        else
+        {
+            cout << "nRecords in descending order of roll number are:" << endl;
+            Node *currentNode = START;
+            while (currentNode->next != NULL)
+                currentNode = currentNode->next;
+
+            while (currentNode != NULL)
+            {
+                cout << currentNode->noMhs << " " << currentNode->name << endl;
+                currentNode = currentNode->prev;
+            }
+        }
+    }
+
+void searchData()
+{
+    if (listEmpty() == true)
+    {
+        cout << "\nList is empty" << endl;
+    
+    }
+    Node *prev, *curr;
+    prev = curr = NULL;
+    cout << "\nEnter the roll number of the student whose record you want to search: ";
+    int num;
+    cin >> num;
+    if (search(num, &prev, &curr) == false)
+       cout << "\nRecord not found" << endl;
+   else
+   {
+    cout << "\nRecord found" << endl;
+    cout << "\nRoll number: " << curr->noMhs << endl;
+    cout << "\nName: " << curr->name << endl;
+   }
+}
+
+int main()
+{
+    // perulangan selama bernilai benar untuk program utama double linked list
+    while (true)
+    {
+        try
+        {
+            cout << "\nMenu" << endl;
+            cout << "1. Add a record to the list" << endl;
+            cout << "2. Delete a record from the list" << endl;
+            cout << "3. View all records in the ascending order of roll numbers" << endl;
+            cout << "4. View all records in the descending order of roll numbers" << endl;
+            cout << "5. Search for a record in the list" << endl;
+            cout << "6. Exit" << endl;
+            cout << "\nEnter your choice (1-6): ";
+            char ch;
+            cin >> ch;
         }
     }
 }
